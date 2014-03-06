139386dbbb3cee7dc8ec687bfdadba6a7cda4d7a
todd-b <tbrunsvold@gmail.com>
2014-03-06 11:03:03 -0500

    javascript removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,36 +15,36 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
-function loadComments(){
+// function loadComments(){
 
-$.getJSON( "/comments.json", function( data ) {
+// $.getJSON( "/comments.json", function( data ) {
 
-	var string_html;
+// 	var string_html;
 
-	$.each(data, function(i, item){
-		//console.log(data[i]);
-		//console.log(data[i].comment_text);
+// 	$.each(data, function(i, item){
+// 		//console.log(data[i]);
+// 		//console.log(data[i].comment_text);
 		
-		if (data[i].comment_text != ''){
-			string_html += data[i].comment_text + '<br>';
-		}
+// 		if (data[i].comment_text != ''){
+// 			string_html += data[i].comment_text + '<br>';
+// 		}
 		
-	});
+// 	});
 
-	$('#comments').html(string_html)
-});
-}
+// 	$('#comments').html(string_html)
+// });
+// }
 
-function convertSeconds(num_seconds){
-	//1393891200
-	//var timestamp = num_seconds;
-	var date = new Date(num_second]*1000);
+// function convertSeconds(num_seconds){
+// 	//1393891200
+// 	//var timestamp = num_seconds;
+// 	var date = new Date(num_second]*1000);
 
-	var year = date.getFullYear();
-	var month = date.getMonth() + 1;
-	var day = date.getDate();
+// 	var year = date.getFullYear();
+// 	var month = date.getMonth() + 1;
+// 	var day = date.getDate();
 	
 
-	alert(month + "/" + day + "/" + year);
+// 	alert(month + "/" + day + "/" + year);
 	
-}
+// }
